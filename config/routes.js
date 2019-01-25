44e2254fa848d725ab7d9c861f82919e0f9d2d5f
jamespagedev <jamespagedev@outlook.com>
2019-01-25 09:56:41 -0700

    added register endpoint, and converted getJokes() to jokes endpoint

--- config/routes.js
+++ config/routes.js
@@ -1,24 +1,45 @@
+/***************************************************************************************************
+ ******************************************* dependencies ******************************************
+ **************************************************************************************************/
 const axios = require('axios');
+const express = require('express');
+const db = require('../database/helpers/dbHelpers.js');
+const bcrypt = require('bcryptjs');
+const router = express.Router();
 
-const { authenticate } = require('../auth/authenticate');
+/***************************************************************************************************
+ ******************************************** middleware *******************************************
+ **************************************************************************************************/
+const authenticate = require('../auth/authenticate.js');
+const authorize = require('../auth/authorize.js');
+const generateToken = require('../auth/generateToken.js');
 
-module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
-};
-
-function register(req, res) {
+/***************************************************************************************************
+ ********************************************* Endpoints *******************************************
+ **************************************************************************************************/
+router.post('/register', (req, res) => {
   // implement user registration
-}
+  const newUserCreds = req.body;
+
+  newUserCreds.password = bcrypt.hashSync(
+    newUserCreds.password,
+    db.settings.pwdHashLength
+  );
+
+  db.addUser(newUserCreds)
+    .then(Ids => {
+      res.status(201).json({ id: Ids[0] }); // returns the userId created by the database
+    })
+    .catch(err => res.status(500).send(err));
+});
 
 function login(req, res) {
   // implement user login
 }
 
-function getJokes(req, res) {
+router.get('/jokes', (req, res) => {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: 'application/json' }
   };
 
   axios
@@ -29,4 +50,9 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
-}
+});
+
+/***************************************************************************************************
+ ********************************************* export(s) *******************************************
+ **************************************************************************************************/
+module.exports = router;
